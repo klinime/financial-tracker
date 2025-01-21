dc5306aa8341ed8e0caab9364f49c5f597749aca
Kevin Lin <klinime@berkeley.edu>
2025-01-20 23:10:22 -0800

    switched pdfplumber to PyMuPDF and implemented two column page detection

--- src/financial_tracker/pdf.py
+++ src/financial_tracker/pdf.py
@@ -1,203 +1,216 @@
+import logging
+import re
+from typing import Any
+
+import fitz
 import numpy as np
-import pdfplumber
 
-point_threshold = 4.5  # 4.5 points is 1/16 inches
-
-
-def is_same_line(obj1: pdfplumber.page.T_obj, obj2: pdfplumber.page.T_obj) -> bool:
-    assert isinstance(obj1["top"], float)
-    assert isinstance(obj2["top"], float)
-    assert isinstance(obj1["bottom"], float)
-    assert isinstance(obj2["bottom"], float)
-    return (abs(obj1["top"] - obj2["top"]) < point_threshold) and (
-        abs(obj1["bottom"] - obj2["bottom"]) < point_threshold
-    )
+point_threshold = 3.0  # 3 points is 1/24 inches
 
 
-def is_h_aligned_center(
-    obj1: pdfplumber.page.T_obj, obj2: pdfplumber.page.T_obj
+def text_block_equivalent(
+    text_block1: dict[str, Any], text_block2: dict[str, Any]
 ) -> bool:
-    assert isinstance(obj1["top"], float)
-    assert isinstance(obj2["top"], float)
-    assert isinstance(obj1["bottom"], float)
-    assert isinstance(obj2["bottom"], float)
-    return (
-        abs((obj1["top"] + obj1["bottom"]) - (obj2["top"] + obj2["bottom"]))
-        < point_threshold * 2
-    )
+    rect1 = fitz.Rect(text_block1["bbox"])
+    rect2 = fitz.Rect(text_block2["bbox"])
+    overlap = rect1 & rect2
+    rect1_area = rect1.width * rect1.height
+    rect2_area = rect2.width * rect2.height
+    overlap_area = overlap.width * overlap.height
+    # sufficient area must overlap to be considered equivalent spatially
+    if (overlap_area / rect1_area) < 0.5 or (overlap_area / rect2_area) < 0.5:
+        return False
+    # number of lines must be the same
+    if len(text_block1["lines"]) != len(text_block2["lines"]):
+        return False
+    # line text must be the same, barring some minor formatting differences
+    for line1, line2 in zip(text_block1["lines"], text_block2["lines"]):
+        if re.sub(r"\d+ of (\d+)", r"of \1", line1["spans"][0]["text"]) != re.sub(
+            r"\d+ of (\d+)", r"of \1", line2["spans"][0]["text"]
+        ):
+            return False
+    return True
 
 
-def is_aligned_center(obj1: pdfplumber.page.T_obj, obj2: pdfplumber.page.T_obj) -> bool:
-    assert isinstance(obj1["x0"], float)
-    assert isinstance(obj2["x0"], float)
-    assert isinstance(obj1["x1"], float)
-    assert isinstance(obj2["x1"], float)
-    return (
-        abs((obj1["x0"] + obj1["x1"]) - (obj2["x0"] + obj2["x1"])) < point_threshold * 2
-    )
-
-
-def is_aligned_left(obj1: pdfplumber.page.T_obj, obj2: pdfplumber.page.T_obj) -> bool:
-    assert isinstance(obj1["x0"], float)
-    assert isinstance(obj2["x0"], float)
-    return abs(obj1["x0"] - obj2["x0"]) < point_threshold
-
-
-def is_equivalent_words(
-    word1: pdfplumber.page.T_obj, word2: pdfplumber.page.T_obj
+def page_blocks_equivalent(
+    page_blocks1: list[dict[str, Any]], page_blocks2: list[dict[str, Any]]
 ) -> bool:
-    # relaxes the requirement that the words are exactly the same
-    # to allow for numeric values matches for patterns like "page 1 of 3"
-    return is_aligned_center(word1, word2) and (
-        (word1["text"] == word2["text"])
-        or (
-            word1["text"].isnumeric()
-            and word2["text"].isnumeric()
-            and (len(word1["text"]) == len(word2["text"]))
-        )
+    return all(
+        text_block_equivalent(block1, block2)
+        for block1, block2 in zip(page_blocks1, page_blocks2)
     )
 
 
-def categorize_two_columns(
-    words: list[pdfplumber.page.T_obj],
-) -> tuple[list[pdfplumber.page.T_obj], list[pdfplumber.page.T_obj]]:
-    min_col_gap = 144  # 144 points is 2 inches
-    left_col: list[list[pdfplumber.page.T_obj]] = []
-    right_col: list[list[pdfplumber.page.T_obj]] = []
-    left_col_words: list[pdfplumber.page.T_obj] = [words[0]]
-    right_col_words: list[pdfplumber.page.T_obj] = []
-    # cartesian product of scenarios:
-    # [left column, right column] x [start x [prev end left, prev end right], continuation]
-    for word in words[1:]:
-        if (
-            left_col_words
-            and is_same_line(left_col_words[-1], word)
-            and abs(word["x0"] - left_col_words[-1]["x1"]) < min_col_gap
-        ):
-            # left column x continuation
-            left_col_words.append(word)
-        elif (
-            not right_col_words
-            and is_same_line(left_col_words[-1], word)
-            and (not right_col or is_aligned_left(right_col[0][0], word))
-        ):
-            # right column x start x prev end left
-            right_col_words.append(word)
-        elif (
-            right_col_words
-            and is_same_line(right_col_words[-1], word)
-            and abs(word["x0"] - right_col_words[-1]["x1"]) < min_col_gap
-        ):
-            # right column x continuation
-            right_col_words.append(word)
-        elif right_col_words and is_aligned_left(left_col_words[0], word):
-            # left column x start x prev end right
-            left_col.append(left_col_words)
-            right_col.append(right_col_words)
-            left_col_words = [word]
-            right_col_words = []
-        elif not right_col_words and is_aligned_left(left_col_words[0], word):
-            # left column x start x prev end left
-            left_col.append(left_col_words)
-            left_col_words = [word]
-        elif (
-            right_col_words
-            and not is_same_line(right_col_words[-1], word)
-            and is_aligned_left(right_col[0][0], word)
-        ):
-            # right column x start x prev end right
-            right_col.append(right_col_words)
-            right_col_words = [word]
-        else:
-            break
+def sorted_header_blocks(text_blocks: list[dict[str, Any]]) -> list[dict[str, Any]]:
+    # sort by bottom of the block because excluding header goes from bottom to top
+    return sorted(text_blocks, key=lambda block: block["bbox"][3])
 
-    # append the remaining lines
-    if left_col_words:
-        left_col.append(left_col_words)
-    if right_col_words:
-        right_col.append(right_col_words)
 
-    print("\nDebug:")
-    print("Left column:")
-    for left_line in left_col:
-        print(" ".join(word["text"] for word in left_line))
-    print("Right column:")
-    for right_line in right_col:
-        print(" ".join(word["text"] for word in right_line))
+def sorted_footer_blocks(text_blocks: list[dict[str, Any]]) -> list[dict[str, Any]]:
+    # sort by top of the block because excluding footer goes from top to bottom
+    return sorted(text_blocks, key=lambda block: block["bbox"][1], reverse=True)
 
-    # hardcoded assumption that there are at least two lines in each column
-    if len(left_col) < 2:
-        return [], []
-    return [word for col_words in left_col for word in col_words], [
-        word for col_words in right_col for word in col_words
+
+def pdf_margin_offsets(doc: fitz.Document) -> tuple[int, int]:
+    logger = logging.getLogger(__name__)
+
+    # reference text blocks assumes maximally 2 pages before the start of the content page
+    # page 0: cover, page 1: terms or summary, page 2: content
+    ref_page_count = min(doc.page_count, 3)
+    ref_page_blocks = [
+        [
+            block
+            for block in doc[page_num].get_text("dict")["blocks"]
+            if block["type"] == 0
+        ]
+        for page_num in range(ref_page_count)
     ]
-
-
-def get_fixed_offset(
-    objs1: list[pdfplumber.page.T_obj],
-    objs2: list[pdfplumber.page.T_obj],
-    header: bool = True,
-) -> float:
-    candidate_objects = []
-    for obj1, obj2 in zip(
-        objs1 if header else objs1[::-1], objs2 if header else objs2[::-1]
-    ):
-        if is_equivalent_words(obj1, obj2):
-            candidate_objects.append(obj1)
-            candidate_objects.append(obj2)
-        else:
-            # to be considered a candidate for header/footer, entire lines must be equivalent
-            while candidate_objects:
-                if is_same_line(candidate_objects[-1], obj1) or is_same_line(
-                    candidate_objects[-1], obj2
-                ):
-                    candidate_objects.pop()
+    header_offsets = []
+    footer_offsets = []
+    for page_num in range(1, doc.page_count):
+        # dedplicate equivalence checks between the reference pages
+        cur_ref_blocks = [
+            page_block
+            for ref_page_num, page_block in enumerate(ref_page_blocks)
+            if page_num > ref_page_num
+        ]
+        page_text_blocks = [
+            block
+            for block in doc[page_num].get_text("dict")["blocks"]
+            if block["type"] == 0
+        ]
+        header_offset = -1
+        footer_offset = -1
+        # equivalent to prefix / suffix matches against each reference text block
+        for ref_text_blocks in cur_ref_blocks:
+            for ref_text_block, page_text_block in zip(
+                sorted_header_blocks(ref_text_blocks),
+                sorted_header_blocks(page_text_blocks),
+            ):
+                if text_block_equivalent(ref_text_block, page_text_block):
+                    header_offset = page_text_block["bbox"][3]
                 else:
                     break
-            break
-    if not candidate_objects:
-        return 0 if header else -1
-    bbox = pdfplumber.utils.objects_to_bbox(candidate_objects)
-    assert isinstance(bbox[1], float)
-    assert isinstance(bbox[3], float)
-    return bbox[3] if header else bbox[1]  # bottom or top
+            for ref_text_block, page_text_block in zip(
+                sorted_footer_blocks(ref_text_blocks),
+                sorted_footer_blocks(page_text_blocks),
+            ):
+                if text_block_equivalent(ref_text_block, page_text_block):
+                    footer_offset = page_text_block["bbox"][1]
+                else:
+                    break
+        if header_offset != -1:
+            header_offsets.append(header_offset)
+        if footer_offset != -1:
+            footer_offsets.append(footer_offset)
 
-
-def pdf_header_footer_offsets(pages: list[pdfplumber.page.Page]) -> tuple[int, int]:
-    page0_words = pages[0].extract_words()
-    page0_header_left_col, page0_header_right_col = categorize_two_columns(page0_words)
-    candidate_header_offsets = []
-    candidate_footer_offsets = []
-    for page in pages[1:]:
-        words = page.extract_words()
-        header_left_col, header_right_col = categorize_two_columns(words)
-        left_header_offset = get_fixed_offset(page0_header_left_col, header_left_col)
-        if page0_header_right_col or header_right_col:
-            right_header_offset = get_fixed_offset(
-                (
-                    page0_header_right_col
-                    if page0_header_right_col
-                    else page0_header_left_col
-                ),
-                header_right_col if header_right_col else header_left_col,
-            )
-        else:
-            right_header_offset = 0
-        candidate_header_offsets.append(max(left_header_offset, right_header_offset))
-
-        # assumes that the footer has a single column or can be effectively treated as a single column
-        candidate_footer_offsets.append(
-            get_fixed_offset(page0_words, words, header=False)
+    header_offset = np.median(header_offsets) if header_offsets else 0
+    footer_offset = np.median(footer_offsets) if footer_offsets else doc[0].rect.height
+    logger.debug(f"{header_offset=} {footer_offset=}")
+    for page_num in range(ref_page_count):
+        logger.debug(
+            f"removed_header:\n{doc[page_num].get_text(clip=fitz.Rect(0, 0, doc[page_num].rect.width, header_offset))}"
+        )
+        logger.debug(
+            f"removed_footer:\n{doc[page_num].get_text(clip=fitz.Rect(0, footer_offset, doc[page_num].rect.width, doc[page_num].rect.height))}"
         )
 
-    return np.median(candidate_header_offsets), np.median(candidate_footer_offsets)
+    return header_offset, footer_offset
+
+
+def is_two_column_page(page: fitz.Page) -> bool:
+    text_blocks = [
+        block for block in page.get_text("dict")["blocks"] if block["type"] == 0
+    ]
+    if not text_blocks:
+        return False
+    left_blocks = [
+        block for block in text_blocks if block["bbox"][2] < page.rect.width / 2
+    ]
+    right_blocks = [
+        block for block in text_blocks if block["bbox"][0] > page.rect.width / 2
+    ]
+    if not left_blocks or not right_blocks:
+        return False
+    left_boundary = float(
+        np.quantile([block["bbox"][0] for block in left_blocks], 0.25)
+    )
+    right_boundary = float(
+        np.quantile([block["bbox"][2] for block in right_blocks], 0.75)
+    )
+    center = (left_boundary + right_boundary) / 2
+    # weighted distance to the center of the page based on the area of the block
+    left_blocks_areas = [
+        (block["bbox"][2] - block["bbox"][0]) * (block["bbox"][3] - block["bbox"][1])
+        for block in left_blocks
+    ]
+    right_blocks_areas = [
+        (block["bbox"][2] - block["bbox"][0]) * (block["bbox"][3] - block["bbox"][1])
+        for block in right_blocks
+    ]
+    left_dist_weighted = sum(
+        [
+            (center - block["bbox"][2]) * area
+            for block, area in zip(left_blocks, left_blocks_areas)
+        ]
+    ) / sum(left_blocks_areas)
+    right_dist_weighted = sum(
+        [
+            (block["bbox"][0] - center) * area
+            for block, area in zip(right_blocks, right_blocks_areas)
+        ]
+    ) / sum(right_blocks_areas)
+    # arbitrary thresholds - too small = no clear separation between columns, too large = too sparse
+    assert isinstance(left_dist_weighted, float)
+    assert isinstance(right_dist_weighted, float)
+    return (
+        (left_dist_weighted > point_threshold)
+        and (left_dist_weighted < point_threshold * 8)
+        and (right_dist_weighted > point_threshold)
+        and (right_dist_weighted < point_threshold * 8)
+    )
+
+
+def extract_pdf_pages(doc: fitz.Document) -> list[fitz.Page]:
+    logger = logging.getLogger(__name__)
+
+    pages: list[fitz.Page] = []
+    header_offset, footer_offset = pdf_margin_offsets(doc)
+    for page in doc:
+        if (header_offset > 0) or (footer_offset > 0):
+            page.set_cropbox(
+                fitz.Rect(0, header_offset, page.rect.width, footer_offset)
+            )
+        if not page.get_contents():
+            continue
+        # assumes that two column pages do not contain important information
+        if is_two_column_page(page):
+            logger.debug(f"Page excluded: {page.number=}")
+            continue
+        pages.append(page)
+    return pages
 
 
 if __name__ == "__main__":
-    import glob
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
 
-    for pdf_path in glob.glob("../../data/*.pdf"):
-        with pdfplumber.open(pdf_path) as pdf:
-            print("\nProcessing:", pdf_path)
-            header_offset, footer_offset = pdf_header_footer_offsets(pdf.pages)
-            print("\nHeader offset:", header_offset, "Footer offset:", footer_offset)
+    import pathlib
+
+    docs = [
+        fitz.open(str(pdf_path))
+        for pdf_path in pathlib.Path("../../data").glob("*.pdf", case_sensitive=False)
+    ]
+    pages: list[fitz.Page] = []
+    for doc in docs:
+        pages.extend(extract_pdf_pages(doc))
+
+    output_pdf = fitz.open()
+    for page in pages:
+        new_page = output_pdf.new_page(width=page.rect.width, height=page.rect.height)
+        new_page.show_pdf_page(new_page.rect, page.parent, page.number)
+    output_pdf.save("output.pdf")
+    output_pdf.close()
+
+    for doc in docs:
+        doc.close()
